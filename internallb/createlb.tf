844d7841a2015465dd4d79cddddc26164d18f14c
paridamalay81 <malay.parida@genmills.com>
2022-07-23 19:30:03 +0530

    portrange

--- internallb/createlb.tf
+++ internallb/createlb.tf
@@ -30,8 +30,9 @@ resource "google_compute_global_forwarding_rule" "frontend-http"{
     ip_address = google_compute_global_address.saticipForLB.id
     network = "default"
     load_balancing_scheme = "EXTERNAL_MANAGED"
+    port_range = "80"
 }
-resource "google_compute_target_http_proxy" "http-proxy" {
+resource "  proxy" "http-proxy" {
   name    = "http-proxy"
   url_map = google_compute_url_map.http-url.id
 }
