c14fed80145c30aab757e9f01f165feead9b929e
paridamalay81 <malay.parida@genmills.com>
2022-08-10 19:46:51 +0530

    again

--- internallb/practice/variable.tf
+++ internallb/practice/variable.tf
@@ -2,14 +2,3 @@ variable "instance_name" {
   type = list(string)
   default = [ "MGOWICLMQ3","JFBWICLMQ4" ]
 }
-variable "instance_zone" {
-  type = string
-  validation {
-    condition=substr(var.instance_name,0,3)=="mgo"
-    value="us-east1-a"
-  }
-  validation {
-    condition=substr(var.instance_name,0,3)=="jfb"
-    value="us-east1-c"
-  }
-}
