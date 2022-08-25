877812d2b776bdb7c419ebba29a32821e3013879
paridamalay81 <malay.parida@genmills.com>
2022-08-25 19:39:13 +0530

    create vm instances and modify for variable and deleted locals variables again

--- internallb/modules/create-vm-machines/main.tf
+++ internallb/modules/create-vm-machines/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "vm-machines" {
   name = "vm-instance-${count.index}"
   machine_type = "e2-medium"
-  zone = lookup(var.instance_zone,substr(var.instance_name[count.index],0,3) == "mgo" ? 1 : 2,"us-west1")
+  zone = lookup(var.instance_zone,substr(var.instance_name[${count.index}],0,3) == "mgo" ? 1 : 2,"us-west1")
   tags = ["webserver"]
   count = var.instance_name  
   boot_disk {
