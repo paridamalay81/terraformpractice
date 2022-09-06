16d90664b40413139d1c9992a27ea0194f158ffd
paridamalay81 <malay.parida@genmills.com>
2022-09-06 19:33:05 +0530

    internal lb tcp modified

--- internallb/modules/internail-lb-tcp/main.tf
+++ internallb/modules/internail-lb-tcp/main.tf
@@ -74,3 +74,28 @@ resource "google_compute_instance_group" "instance_group" {
   }
   zone = "us-central1-a"
 }
+resource "google_compute_instance" "instance" {
+   name = "vm-instance-test-lb"
+  machine_type = "e2-medium"
+  zone = "us-central1-a"
+  tags = ["webserver"]
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20220303"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      
+    }
+  }
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    sudo yum update httpd
+    sudo yum install httpd -y
+    sudo systemctl start httpd
+    echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
+  EOF
+  
+}
