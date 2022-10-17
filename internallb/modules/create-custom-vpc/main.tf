181afca538084b02ee187d9a5acef1c3825a740b
paridamalay81 <malay.parida@genmills.com>
2022-10-17 17:01:12 +0530

    add for custom VPC

---
+++ internallb/modules/create-custom-vpc/main.tf
@@ -0,0 +1,13 @@
+locals {
+  subnet-range = lookup(var.subnets,"${var.environment}-${var.spec_region}")
+}
+resource "google_compute_network" "custom_network" {
+  name                    = "custom-network"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "custom_network_subnetwork" {
+  name          = "custom_network_subnetwork-${var.environment}"
+  ip_cidr_range = local.subnet-range
+  region        = var.region
+  network       = google_compute_network.custom-network.id
+ }
---
+++ internallb/modules/create-custom-vpc/variable.tf
@@ -0,0 +1,20 @@
+variable "environment" {
+    type = "string"
+    default = "dev"
+}
+variable "subnets" {
+  type = "map"
+  default = {
+    "dev-us-east1" = "10.0.1.0/28"
+    "dev-us-central" = "10.0.2.0/28"
+    "prod-us-east1" = "10.0.3.0/28"
+    "prod-us-central1" = "10.0.4.0/28"
+  }
+}
+variable "spec_region" {
+  type = "string"
+  validation {
+    condition = ! (var.spec_region == "us-east1" ||  var.spec_region == "us-central1")
+    error_message = "Region should be us-east1/us-central1"
+  }
+}
