284922659a6b66ebcfcdeae1021d533f3b55d192
paridamalay81 <malay.parida@genmills.com>
2022-11-28 09:53:44 +0530

    add for testing

---
+++ internallb/test-for-self/main.tf
@@ -0,0 +1,27 @@
+locals {
+  myname="webservers"
+}
+resource "google_compute_instance" "vm-machines" {
+  name = "vm-instance-${count.index}"
+  machine_type = "e2-medium"
+  zone = "us-central1-a"
+  tags = [local.myname]
+  count = length(var.instance_name)
+  metadata = {
+    ssh-keys="${var.username}:${file("~/.ssh/id_rsa.pub")}"
+  }
+  labels = {
+    local.myname = "true"
+  }
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20220303"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+}
