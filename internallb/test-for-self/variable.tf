4507a0c0563dce1f4760dd4f9379a60b5bd8e4e1
paridamalay81 <malay.parida@genmills.com>
2022-11-28 10:40:43 +0530

    add for testing

---
+++ internallb/test-for-self/variable.tf
@@ -0,0 +1,16 @@
+#Variables for creating AS2 instances
+
+/* Neeed to pass the instance names to create as ["instance1","instance2"] for creating first and second instance in GCP 
+       and CVO mount will be attached to the first instance only. */
+variable instance_list {
+  type = list(string)
+}
+variable "machine_type" {
+  type = string
+  default = "n2-highmem-4"
+}
+variable "vm_source_image" {
+  type = string
+  default = "Need to enter"
+}
+#Variables for creating AS2 instances
