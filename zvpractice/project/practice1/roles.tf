ca0076bfd8259360aa5af6c10647c793e30f8ed5
paridamalay81 <malay.parida@genmills.com>
2023-10-20 18:49:31 +0530

    compute instance

---
+++ zvpractice/project/practice1/roles.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "app-server" {
+  name = "app-server-with-storage"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
