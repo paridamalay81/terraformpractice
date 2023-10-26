0b7a9e977c64b5e7cae8d7ec7411b1c5a09e15de
paridamalay81 <malay.parida@genmills.com>
2023-10-26 11:15:25 +0530

    data

--- zvpractice/module/iam/main.tf
+++ zvpractice/module/iam/main.tf
@@ -6,7 +6,7 @@ data "google_project_iam_policy" "pro_policy" {
   project  = "wise-shell-330415"
 }
 output "pro_policy" {
-  value = google_project_iam_policy.pro_policy
+  value = data.google_project_iam_policy.pro_policy
 }
 /*resource "google_service_account" "app-server-sa" {
   account_id   = "app-server-sa"
