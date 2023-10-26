28deaf5a39c914891ddc7696f94e7b61d44b76b8
paridamalay81 <malay.parida@genmills.com>
2023-10-26 11:11:02 +0530

    change iam

--- zvpractice/module/iam/main.tf
+++ zvpractice/module/iam/main.tf
@@ -2,7 +2,10 @@ provider "google" {
   project     = "wise-shell-330415"
   region      = "us-central1"
 }
-resource "google_service_account" "app-server-sa" {
+data "google_project_iam_policy" "name" {
+  project  = "wise-shell-330415"
+}
+/*resource "google_service_account" "app-server-sa" {
   account_id   = "app-server-sa"
   display_name = "A service account that create for storage"
-}
+}*/
