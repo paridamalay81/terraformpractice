2cd33054fc56ef1a3a3e340dede89e787375937f
paridamalay81 <malay.parida@genmills.com>
2023-11-13 09:49:27 +0530

    commit4

--- zvpractice/module/test/main.tf
+++ zvpractice/module/test/main.tf
@@ -1,13 +1,22 @@
-variable "disk_prod_tst" {
-  type = map(any)
+variable "iam-user-policy-map" {
   default = {
-    disk1 = ["pd-ssd","80","us-central1-a"]
-    disk2 = ["pd-ssd","180","us-central1-a"]
-  }  
+    "user 1" = ["policy1", "policy2"],
+    "user 2" = ["policy1"]
+  }
 }
+
 locals {
-  local-disk={for disk,prop-list in var.disk_prod_tst:upper(disk)=> disk}
+  association-map = merge([
+    for user, policies in var.iam-user-policy-map : {
+      for policy in policies :
+        "${user}-${policy}" => {
+          "user"   = user
+          "policy" = policy
+        }
+    }
+  ]...)
 }
-output "disk_prod" {
-  value = local.local-disk
+
+output "association-map" {
+  value = local.association-map
 }
