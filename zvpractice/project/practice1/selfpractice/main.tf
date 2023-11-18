1bb91d1831cde0c8681e5e1a8746107f2a8bc0d6
paridamalay81 <malay.parida@genmills.com>
2023-11-18 19:49:58 +0530

    local

--- zvpractice/project/practice1/selfpractice/main.tf
+++ zvpractice/project/practice1/selfpractice/main.tf
@@ -2,16 +2,17 @@ provider "google" {
   project     = "wise-shell-330415"
   region      = "us-central1"
 }
-locals {  
-  disk_prod = {   
-    disk1 = ["pd-ssd","80","us-central1-a"]
-    disk2 = ["pd-ssd","180","us-central1-a"]
-  }
-  disk_prod_attr = ["type","size","zoner"] 
-  prod_disk_helper={    
-    for disk,disk_attr in local.disk_prod:disk=>zipmap(local.disk_prod_attr,disk_attr)    
+variable "var_env" {
+  type = map(any)
+  default = {
+    prod=2
+    dev=1
   }
 }
-output "disk_attr_map" {
-  value = local.prod_disk_helper
+locals{
+    env="prod"
+    disk_count=local.env=="prod"?lookup(var.var_env,local.env):lookup(var.var_env,local.env)
+}
+output "diskcount" {
+  value=local.disk_count
 }
