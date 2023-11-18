ff607994e01191abf39dbf9991326b6aebad4e38
paridamalay81 <malay.parida@genmills.com>
2023-11-18 17:47:27 +0530

    selfpractice

---
+++ zvpractice/project/practice1/selfpractice/main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project     = "wise-shell-330415"
+  region      = "us-central1"
+}
+locals {
+  machine_type = lookup(var.machine_type,var.environment)
+  disk_prod = {   
+    disk1 = ["pd-ssd","80","us-central1-a"]
+    disk2 = ["pd-ssd","180","us-central1-a"]
+  }
+  disk_prod_attr = ["type","size","zone"] 
+ 
+}
+output "disk_attr_map" {
+  value = local.disk_prod_attr
+}
