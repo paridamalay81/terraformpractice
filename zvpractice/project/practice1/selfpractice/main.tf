cc5385c1113bac14595466942c1e32a4d063d6e8
paridamalay81 <malay.parida@genmills.com>
2023-11-18 18:12:56 +0530

    aded zoner

--- zvpractice/project/practice1/selfpractice/main.tf
+++ zvpractice/project/practice1/selfpractice/main.tf
@@ -7,7 +7,7 @@ locals {
     disk1 = ["pd-ssd","80","us-central1-a"]
     disk2 = ["pd-ssd","180","us-central1-a"]
   }
-  disk_prod_attr = ["type","size","zone"] 
+  disk_prod_attr = ["type","size","zoner"] 
   prod_disk_helper={    
     for disk,disk_attr in local.disk_prod:disk=>zipmap(local.disk_prod_attr,disk_attr)    
   }
