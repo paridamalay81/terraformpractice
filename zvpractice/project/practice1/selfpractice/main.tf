02fe871cb3aec41a53864575f8dab954c0e739ac
paridamalay81 <malay.parida@genmills.com>
2023-11-26 19:05:33 +0530

    compute instance ceation

--- zvpractice/project/practice1/selfpractice/main.tf
+++ zvpractice/project/practice1/selfpractice/main.tf
@@ -16,3 +16,34 @@ locals{
 output "diskcount" {
   value=local.disk_count
 }
+resource "google_compute_disk" "compute_disk"{
+  count = local.disk_count
+  name = "compute_disk{count.index}"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  size = 60
+}
+  output "cdisk" {
+  value=google_compute_disk.compute_disk
+}
+resource "google_compute_instance" "instance_web" {
+  name = "test-temp-instance"
+  machine_type = "n2-standard-2"
+  network_interface {
+    network = "default"
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+  dynamic "attached_disk" {
+    for_each = [google_compute_disk.compute_disk]
+    content {
+      source = each.name
+    }    
+  }
+}
